bef2d98b8f178d68a3e4f7621d173ccc61e474b9
LeoUnglaub <leo@leo-unglaub.net>
2011-01-18 23:45:40 +0000

    - tag ER version 1.0.0
    git-svn-id: http://svn.lu-hosting.net/open_layers@3 077353ab-9dbe-4e92-b3d4-10c2d49f4130

---
+++ tags/1.0.0/system/modules/open_layers/config/database.sql
@@ -0,0 +1,16 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+-- 
+-- Table `tl_layout`
+-- 
+
+CREATE TABLE `tl_layout` (
+  `openlayers` varchar(100) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
