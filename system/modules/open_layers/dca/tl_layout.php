950bc03bd252f8c142d80924bcaf7ba9b2396dfe
Leo Unglaub <leo@leo-unglaub.net>
2012-08-14 19:49:23 +0200

    - add OpenLayers 2.12 to the extension

--- system/modules/open_layers/dca/tl_layout.php
+++ system/modules/open_layers/dca/tl_layout.php
@@ -10,12 +10,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
  * Software Foundation website at <http://www.gnu.org/licenses/>.
@@ -43,7 +43,7 @@ $GLOBALS['TL_DCA']['tl_layout']['fields']['openlayers'] = array
 	'label'		=> &$GLOBALS['TL_LANG']['tl_layout']['openlayers'],
 	'exclude'	=> true,
 	'inputType'	=> 'select',
-	'options'	=> array('remote_stable', '2_11', '2_10', '2_9', '2_8', '2_7'),
+	'options'	=> array('remote_stable', '2_12', '2_11', '2_10', '2_9', '2_8', '2_7'),
 	'reference'	=> $GLOBALS['TL_LANG']['tl_layout'],
 	'eval'		=> array('tl_class'=>'m12', 'includeBlankOption'=>true)
 );
